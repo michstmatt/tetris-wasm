628b9aeda6e1c7d16c55231a7e308fafbfa0bc74
Matt Pasco <michstmatt@gmail.com>
2022-01-04 15:13:54 -0700

    renaming main to tetris

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 FILE="tetris.wasm"
-emcc ./src/main.c -s STANDALONE_WASM -Oz -o $FILE --no-entry -s "EXPORTED_FUNCTIONS=_initGame,_getScore, _updateGame"
+emcc ./src/tetris.c -s STANDALONE_WASM -Oz -o $FILE --no-entry -s "EXPORTED_FUNCTIONS=_initGame,_getScore, _updateGame"
 mv $FILE ./wasm/$FILE
